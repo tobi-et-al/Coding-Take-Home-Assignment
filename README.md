44de05b5a1152f752c9bb3e1b5946edc8486c65b
Pan fried denzel <toubieogunleye@live.com>
2022-01-27 15:41:32 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# coding-take-home-assignment
+# Coding-Take-Home-Assignment
+
 Created with CodeSandbox
+
+#### Installation guide
+
+- copy example.env to .env
+- update .env with the correct value for GIPHY_API_KEY
+- Execute `yarn` to install the node modules
+- Execute `yarn start` to start the application
+
+#### Components added
+
+- List - to display out results
+
+#### Potential additional features
+
+- Pagination
+- Caching of previous request
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+import DisplayList from "./components/DisplayList";
+
+export default function App() {
+  let [list, setList] = useState([]);
+  let [errorMsg, setErrorMsg] = useState(null);
+
+  const fetchData = async () => {
+    const searchString = "random";
+
+    const url = `${process.env.GIPHY_SEARCH_URL}?api_key=${process.env.GIPHY_API_KEY}&q=${searchString}&limit=25&offset=0&rating=g&lang=en`;
+    let result = null;
+    try {
+      result = await fetch(url);
+    } catch (e) {
+      setErrorMsg(`Error pulling data, ${e.message}. Please try again later`);
+      return [];
+    }
+
+    if (result.ok) {
+      return result.json();
+    }
+    setErrorMsg(`Error pulling data, Please try again later`);
+    return [];
+  };
+
+  useEffect(async () => {
+    list = await fetchData();
+    setList(list.data);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="d-flex flex-wrap justify-content-start">
+        <div className="col-sm-6 offset-3">
+          {errorMsg && <p className="text-error">{errorMsg}</p>}
+          {<DisplayList data={list}></DisplayList>}
+        </div>
+      </div>
+    </div>
+  );
+}
